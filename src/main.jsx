f5a08a8c12f39ab704f13dd3064be064a19d0827
mahdi <mahasr2040@gmail.com>
2025-10-06 15:31:41 +0330

    change route  in the main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -10,11 +10,12 @@ import Corse from "./Corse.jsx";
 createRoot(document.getElementById('root')).render(
   //  <StrictMode>
   <Router basename="/portfolio1/">
-      <Routes>
+    <Home/>
+      {/* <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/corses" element={<Corse/>} />
-          {/* <Route path="/about"  /> */}
-       </Routes>
+          {/* <Route path="/about"  />
+       </Routes> */}
   </Router>
     // <App />
     // <Home />

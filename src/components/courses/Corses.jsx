36734ce95a5047679ad6258f1a7cf42c30dfab92
mahdi <mahasr2040@gmail.com>
2026-05-29 19:32:10 +0330

    update page route2 Corses.jsx

--- src/components/courses/Corses.jsx
+++ src/components/courses/Corses.jsx
@@ -4,7 +4,7 @@ export default function Corses(){
 
     return(
         <div className='start'>
-            <h1 className='hh1'>Hello world , welcome to web corses</h1>
+            <h1 className='hh1'>Hello world , welcome to web Mahdi Rush</h1>
         </div>
     )
 
